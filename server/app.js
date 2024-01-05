0ecdeeaca85ed29c088ca5360943511ec757c90f
Билгүүн Баттөгс <LP230078@G301-CC0173.local>
2024-01-05 15:20:00 +0800

    deploy-2

--- server/app.js
+++ server/app.js
@@ -20,6 +20,10 @@ app.use("/api", transRoutes);
 app.use("/api/image", imageRoutes);
 app.use("/images", express.static(path.join(__dirname, "images")));
 
+app.get("/", (req, res) => {
+  res.send("Welcome API");
+});
+
 app.listen(PORT, () => {
   console.log("SERVER IS STARTED ON", PORT);
 });
